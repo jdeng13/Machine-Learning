898dff9e297aa64541e8750197d6415a538d1881
Jianru Deng <38848872+jdeng13@users.noreply.github.com>
2018-11-24 23:25:24 -0800

    Update Multivariate_Gaussian_Rand_Number_-1_test.py

--- Multivariate_Gaussian_Rand_Number_-1_test.py
+++ Multivariate_Gaussian_Rand_Number_-1_test.py
@@ -86,24 +86,24 @@ def main():
     fignum = 1
 
     # fit the model
+    # Three different types of SVM-Kernels are displayed below. The polynomial and RBF(Radial Basis Function) are especially useful when the data-points are not linearly separable.
+
     for kernel in ('linear', 'poly', 'rbf'):
-        clf = svm.SVC(C=1, kernel=kernel, gamma=10)
+        clf = svm.SVC(C=1, kernel=kernel, gamma=0.1)
         clf.fit(X, Y)
 
         # plot the line, the points, and the nearest vectors to the plane
         plt.figure(fignum, figsize=(4, 3))
         plt.clf()
 
-        plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80,
-                    facecolors='none', zorder=10, edgecolors='k')
-        plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired,
-                    edgecolors='k')
+        plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80,facecolors='none', zorder=10, edgecolors='k')
+        plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired, edgecolors='k')
 
         plt.axis('tight')
-        x_min = -3
-        x_max = 3
-        y_min = -3
-        y_max = 3
+        x_min = -8
+        x_max = 8
+        y_min = -8
+        y_max = 8
 
         XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
         Z = clf.decision_function(np.c_[XX.ravel(), YY.ravel()])
@@ -112,9 +112,7 @@ def main():
         Z = Z.reshape(XX.shape)
         plt.figure(fignum, figsize=(4, 3))
         plt.pcolormesh(XX, YY, Z > 0, cmap=plt.cm.Paired)
-        plt.contour(XX, YY, Z, colors=['k', 'k', 'k'], linestyles=['--', '-', '--'],
-                    levels=[-.5, 0, .5])
-
+        plt.contour(XX, YY, Z, colors=['k', 'k', 'k'], linestyles=['--', '-', '--'], levels=[-0.4, 0, 0.4])
         plt.xlim(x_min, x_max)
         plt.ylim(y_min, y_max)
 
